77705f448b63d2372eb7a1ae7a6cac358dc7a07e
Jordan883 <jfernan6@stevens.edu>
2020-09-30 21:55:01 -0400

    Fixed test cases per new error message values

--- TestRepoCheck.py
+++ TestRepoCheck.py
@@ -23,16 +23,16 @@ class TestRepoCheck(unittest.TestCase):
         self.assertEqual(str(check_repos("octocat")),"{'boysenberry-repo-1': 4, 'git-consortium': 6, 'hello-worId': 1, 'Hello-World': 3, 'linguist': 30, 'octocat.github.io': 4, 'Spoon-Knife': 3, 'test-repo1': 1}", 'octocat should return a full list of repos.')
 
     def testJunk(self): 
-        self.assertEqual(str(check_repos("{{*JUNK*}}")),"{'User query {{*JUNK*}} failed; code 404': 0}", '{{*JUNK*}} should fail to connect with code 404.')
+        self.assertEqual(str(check_repos("{{*JUNK*}}")),"{'User query {{*JUNK*}} failed; code 404': -1}", '{{*JUNK*}} should fail to connect with code 404.')
     
     def testBlank(self): 
-        self.assertEqual(str(check_repos("")),"{'User query failed; code 404': 0}", '<Blank> should fail to connect with code 404.')
+        self.assertEqual(str(check_repos("")),"{'User query failed; code 404': -1}", '<Blank> should fail to connect with code 404.')
     
     def testZero(self): 
-        self.assertEqual(str(check_repos(0)),"{'User query failed; code 404': 0}", '<Blank> should fail to connect with code 404.')
+        self.assertEqual(str(check_repos(0)),"{'User query failed; code 404': -1}", '<Blank> should fail to connect with code 404.')
     
     def testDict(self): 
-        self.assertEqual(str(check_repos({"Apple": 1})),"{'User query failed; code 404': 0}", '<Blank> should fail to connect with code 404.')
+        self.assertEqual(str(check_repos({"Apple": 1})),"{'User query failed; code 404': -1}", '<Blank> should fail to connect with code 404.')
 
 if __name__ == '__main__':
     print('Running unit tests')
