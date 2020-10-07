cb570e9b905d7c7f929c77a834fd91b9181c0d37
Jordan883 <jfernan6@stevens.edu>
2020-10-07 17:30:18 -0400

    Commit for HW05a, adding mocking for unit tests

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 I pledge my honor that I have abided by the Stevens Honor System. - Jordan Fernandes
 
-[![build status of master](https://travis-ci.org/Jordan883/repo_and_comm_check.svg?branch=master)](https://travis-ci.org/Jordan883/repo_and_comm_check)
+[![build status of HW05a_Mocking](https://travis-ci.org/Jordan883/repo_and_comm_check.svg?branch=HW05a_Mocking)](https://travis-ci.org/Jordan883/repo_and_comm_check)
--- TestRepoCheck.py
+++ TestRepoCheck.py
@@ -11,6 +11,10 @@ I pledge my honor that I have abided by the Stevens Honor System. - Jordan Ferna
 """
 
 import unittest
+import json
+
+from unittest.mock import MagicMock as Mock
+from unittest.mock import patch
 
 from RepoCheck import check_repos
 
@@ -19,13 +23,39 @@ from RepoCheck import check_repos
 
 class TestRepoCheck(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
-    def testOctocat(self): 
+
+    @patch('requests.get')
+    def testOctocat(self, injectedMock): 
+        results = [Mock(), Mock(), Mock(), Mock(), Mock(), Mock(), Mock(), Mock(), Mock()]
+        results[0].json.return_value = json.loads('[{"name": "boysenberry-repo-1"}, {"name": "git-consortium"}, {"name": "hello-worId"}, {"name": "Hello-World"}, {"name": "linguist"}, {"name": "octocat.github.io"}, {"name": "Spoon-Knife"}, {"name": "test-repo1"}]')
+        results[1].json.return_value = json.loads('[{"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}]')
+        results[2].json.return_value = json.loads('[{"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}]')
+        results[3].json.return_value = json.loads('[{"commit": "a"}]')
+        results[4].json.return_value = json.loads('[{"commit": "a"}, {"commit": "a"}, {"commit": "a"}]')
+        results[5].json.return_value = json.loads('[{"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}]')
+        results[6].json.return_value = json.loads('[{"commit": "a"}, {"commit": "a"}, {"commit": "a"}, {"commit": "a"}]')
+        results[7].json.return_value = json.loads('[{"commit": "a"}, {"commit": "a"}, {"commit": "a"}]')
+        results[8].json.return_value = json.loads('[{"commit": "a"}]')
+        results[0].status_code = 200
+        results[1].status_code = 200
+        results[2].status_code = 200
+        results[3].status_code = 200
+        results[4].status_code = 200
+        results[5].status_code = 200
+        results[6].status_code = 200
+        results[7].status_code = 200
+        results[8].status_code = 200
+        injectedMock.side_effect = results
         self.assertEqual(check_repos('octocat'), {'boysenberry-repo-1': 4, 'git-consortium': 6, 'hello-worId': 1, 'Hello-World': 3, 'linguist': 30, 'octocat.github.io': 4, 'Spoon-Knife': 3, 'test-repo1': 1}, '\'octocat\' should return a full list of repos.')
 
-    def testJunk(self): 
+    @patch('requests.get')
+    def testJunk(self, injectedMock): 
+        injectedMock.return_value.status_code = 404
         self.assertEqual(check_repos('{{*JUNK*}}'), {'User query {{*JUNK*}} failed; code 404': -1}, '\'{{*JUNK*}}\' should fail to connect with code 404.')
     
-    def testBlank(self): 
+    @patch('requests.get')
+    def testBlank(self, injectedMock): 
+        injectedMock.return_value.status_code = 404
         self.assertEqual(check_repos(''), {'User query  failed; code 404': -1}, '<Blank> should fail to connect with code 404.')
     
     def testTwo(self): 
@@ -34,7 +64,10 @@ class TestRepoCheck(unittest.TestCase):
     def testDict(self): 
         self.assertEqual(check_repos({'Apple': 1}), {'Input is not a string': -1}, '{\'Apple\': 1} is not a valid input.')
     
-    def testEmpty(self):
+    @patch('requests.get')
+    def testEmpty(self, injectedMock):
+        injectedMock.return_value.json.return_value = json.loads('{}')
+        injectedMock.return_value.status_code = 200
         self.assertEqual(check_repos('Jordan884'), {}, '\'Jordan884\' should return an empty dictionary.')
 
 if __name__ == '__main__':
